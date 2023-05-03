cc66604ce2cbbc59c15fd05bb2262c63c7702ceb
abeel50 <abeelmuhammad50@gmail.com>
2023-05-03 10:57:37 +0500

    Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LayoutComponent } from './layout/layout.component';
+import { TopNavComponent } from './layout/top-nav/top-nav.component';
+import { SideBarComponent } from './layout/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    TopNavComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
