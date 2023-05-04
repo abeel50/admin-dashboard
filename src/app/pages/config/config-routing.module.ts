09d8f596eaf71df1da578950980f4df30f97cf18
abeel50 <abeelmuhammad50@gmail.com>
2023-05-04 16:16:01 +0500

    Locations Page routing

--- src/app/pages/config/config-routing.module.ts
+++ src/app/pages/config/config-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SliderComponent } from './slider/slider.component';
 import { FeaturedCategoriesComponent } from './featured-categories/featured-categories.component';
+import { LocationsComponent } from './locations/locations.component';
 
 const routes: Routes = [
   { path: '', component: SliderComponent },
   { path: 'slider', component: SliderComponent },
   { path: 'featured-categories', component: FeaturedCategoriesComponent },
+  { path: 'locations', component: LocationsComponent },
 
 ];
 
--- src/app/pages/config/config.module.ts
+++ src/app/pages/config/config.module.ts
@@ -4,9 +4,10 @@ import { FeaturedCategoriesComponent } from './featured-categories/featured-cate
 import { SliderComponent } from './slider/slider.component';
 import { ConfigRoutingModule } from './config-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { SubCategoryTableComponent } from './sub-category-table/sub-category-table.component';
-import { CategoryTableComponent } from './category-table/category-table.component';
-import { NgbdSortableHeader } from './category-table/sortable.directive';
+import { SubCategoryTableComponent } from './common/sub-category-table/sub-category-table.component';
+import { CategoryTableComponent } from './common/category-table/category-table.component';
+import { NgbdSortableHeader } from './common/category-table/sortable.directive';
+import { LocationsComponent } from './locations/locations.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import { NgbdSortableHeader } from './category-table/sortable.directive';
     NgbdSortableHeader
 
   ],
-  declarations: [FeaturedCategoriesComponent, SliderComponent, SubCategoryTableComponent, CategoryTableComponent]
+  declarations: [FeaturedCategoriesComponent, SliderComponent, LocationsComponent,
+    SubCategoryTableComponent, CategoryTableComponent]
 })
 export class ConfigModule { }
