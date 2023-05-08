6aa857b60f9515954e0e4040f9912548979916e7
abeel50 <abeelmuhammad50@gmail.com>
2023-05-08 16:12:40 +0500

    Order Detail/ Invoice

--- src/app/pages/orders/order-detail/order-detail.component.ts
+++ src/app/pages/orders/order-detail/order-detail.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderDetailComponent implements OnInit {
 
+  order = {
+    id: 12345,
+    customerName: 'John Doe',
+    phoneNumber: '555-1234',
+    deliveryAddress: '123 Main St, Anytown USA',
+    deliveryStatus: 'In Transit',
+    deliveryGuy: 'Bob Smith',
+    totalPrice: 129.99,
+    items: [
+      { id: 1, productName: 'Product A', quantity: 2, price: 59.99 },
+      { id: 2, productName: 'Product B', quantity: 1, price: 10.00 },
+      { id: 3, productName: 'Product C', quantity: 3, price: 20.00 }
+    ]
+  };
+
   constructor() { }
 
   ngOnInit() {
+    // const id = this.route.snapshot.paramMap.get('id');
+    // Here you can retrieve the actual order data from your backend API using the provided ID
   }
 
 }
