f847247adfde065b2926680e1954f8290f166faa
abeel50 <abeelmuhammad50@gmail.com>
2023-05-11 16:32:25 +0500

    Package's Form Working

--- src/app/pages/product-packages/product-packages-form/product-packages-form.component.ts
+++ src/app/pages/product-packages/product-packages-form/product-packages-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PRODUCTS } from 'src/app/_constants';
+import { FeaturedCategory, Package, PackageProduct, Product } from 'src/app/_interfaces';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-product-packages-form',
@@ -7,9 +11,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductPackagesFormComponent implements OnInit {
 
-  constructor() { }
+  _products: Product[] = PRODUCTS;
+  isFormSubmit: boolean = false;
+  form: FormGroup;
+  buttonTitle: string = "Save";
+
+  // Sub Form
+  isProductFormSubmit: boolean = false;
+  subForm: FormGroup;
+  subProducts: PackageProduct[] = [];
+
+  constructor(private formBuilder: FormBuilder,) { }
 
   ngOnInit() {
+    this.create();
+    this.createSubForm();
   }
 
+  create() {
+    this.form = this.formBuilder.group({
+      title: [null, [Validators.required]],
+      totalPrice: [null, [Validators.required]],
+      products: []
+    });
+  }
+
+  get f() { return this.form.controls; }
+
+  // Save/edit button
+  onSubmit() {
+    this.isFormSubmit = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+    this.f['products'].setValue(this.subProducts);
+    Swal.fire('', `${JSON.stringify(this.form.value)}`, 'info')
+  }
+
+  // Edit Form Event Catcher
+  editClick(pack: Package) {
+    this.isFormSubmit = false;
+    this.buttonTitle = "Edit";
+    this.form.patchValue(pack);
+    this.subProducts = pack.products;
+  }
+
+
+  createSubForm() {
+    this.subForm = this.formBuilder.group({
+      productID: [null, [Validators.required]],
+      weight: [null, [Validators.required]]
+    });
+  }
+
+  get sf() { return this.subForm.controls; }
+
+
+  // Save button
+  onSubmitSubForm() {
+    this.isProductFormSubmit = true;
+
+    if (this.subForm.invalid) {
+      return;
+    }
+    this.subProducts.push({ id: this.subProducts.length + 1, productID: this.sf['productID'].value, weight: this.sf['weight'].value });
+    this.subForm.reset();
+    this.isProductFormSubmit = false;
+
+  }
+
+
+  deleteSubProduct(e: number) {
+    this.subProducts = this.subProducts.filter(item => item.id !== e);
+  }
 }
