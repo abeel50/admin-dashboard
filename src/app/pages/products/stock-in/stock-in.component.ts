4386e106ead821811854e3dceec7ce34c146056a
abeel50 <abeelmuhammad50@gmail.com>
2023-05-12 17:28:40 +0500

    Stock In form

--- src/app/pages/products/stock-in/stock-in.component.ts
+++ src/app/pages/products/stock-in/stock-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PRODUCTS } from 'src/app/_constants';
+import { Product, StockIn } from 'src/app/_interfaces';
 
 @Component({
   selector: 'app-stock-in',
@@ -7,9 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockInComponent implements OnInit {
 
-  constructor() { }
+  _products: Product[] = PRODUCTS;
+
+  stockData: StockIn[] = [];
+
+  isFormSubmit: boolean = false;
+  form: FormGroup;
+  buttonTitle: string = "Save";
+
+
+  constructor(private formBuilder: FormBuilder,) { }
 
   ngOnInit() {
+    this.create();
   }
+
+  create() {
+    this.form = this.formBuilder.group({
+      productID: [null, [Validators.required]],
+      quantity: [null, [Validators.required]],
+      totalPrice: [{ value: null, disabled: true }, [Validators.required],]
+    });
+  }
+
+  get f() { return this.form.controls; }
+
+  changeQuantity() {
+    let price = PRODUCTS.find(e => e.id === this.f['productID'].value)?.defaultPrice || 0;
+    let qt = this.f['quantity'].value;
+    this.f['totalPrice'].setValue(price * qt);
+  }
+
+  addStock() {
+    this.isFormSubmit = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.stockData.push({
+      id: this.stockData.length + 1, productID: this.f['productID'].value,
+      quantity: this.f['quantity'].value, totalPrice: this.f['totalPrice'].value
+    });
+
+    this.form.reset();
+  }
+
 
 }
